d2876be13cb02703b7a0e93d893d1d53d49baee1
DIYORBEK <diyorbek5300@gmail.com>
2023-01-17 16:43:02 +0500

    test

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://diyorbek.biz';
+export const BASE_URL = 'https://diyorbek.biz';
 export const CONSTANT_2 = 'CONSTANT_2';
 export const CONSTANT_3 = 'CONSTANT_3';
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,8 +1,7 @@
 <template>
-  <div style="background: #f5f4f4; width: 100%; height: 100%">
+  <div style="background: #f5f4f4; width: 100%; height: 100%; padding-bottom: 10px; padding-top: 10px;">
 <v-container style="display: flex; justify-content: center; align-items: center; margin-top: 160px; padding-bottom: 10px">
   <v-card width="600" >
-    <p style="margin-top: 20px"></p>
 <v-card-text align="center" style="font-size: 32px; font-weight: bold">Login</v-card-text>
     <div style="display: flex; justify-content: center; position: absolute; top: -121312321342px">
       <v-alert class="" style="margin-left: 10px;margin-right: 10px" type="error" variant="outlined">
@@ -11,12 +10,12 @@
     </div>
    <form @submit.prevent="login">
      <v-col style="margin-left: 80px">
-       <label class="text-lable">Email:</label>
-       <div style="align-items: center"><input class="inp" type="email" name="email" placeholder="Email" v-model="email"></div>
+       <label class="text-lable"><span class="text-red">*</span>Email:</label>
+       <div style="align-items: center"><input class="inp" type="email" name="email" placeholder="Email" v-model="email" required></div>
      </v-col>
      <v-col style="margin-left: 80px">
-       <label class="text-lable">Password:</label>
-       <div style="align-items: center"><input class="inp" type="password" name="password" placeholder="Password" v-model="password"></div>
+       <label class="text-lable"><span class="text-red">*</span>Password:</label>
+       <div style="align-items: center"><input class="inp" type="password" name="password" placeholder="Password" v-model="password" required></div>
      </v-col>
      <v-col align="center">
        <v-btn width="420" elevation="0" color="success" @click.prevent="login">Login</v-btn>
